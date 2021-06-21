7ae5aa84fdd72f14c338d58b0c959054fa80f69c
vindarel <vindarel@mailz.org>
2021-06-21 15:29:39 +0200

    post hook: git init but no checkout or add remote

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,9 @@
 """Post-project generation hooks"""
 
 if __name__ == '__main__':
-    """Initialize a git repository based on the configured branch and repo"""
-    url    = 'git@github.com:{{ cookiecutter.username }}/{{ cookiecutter.repo_name }}'
-    branch = '{{ cookiecutter.branch }}'
-    remote = '{{ cookiecutter.remote }}'
+    """Initialize a git repository."""
 
     import subprocess
     subprocess.call(('git', 'init'))
-    subprocess.call(('git', 'checkout', '-b', branch))
-    subprocess.call(('git', 'remote', 'add', remote, url))
+
+    print("Project {{ cookiecutter.project_name }} created succesfully.")
