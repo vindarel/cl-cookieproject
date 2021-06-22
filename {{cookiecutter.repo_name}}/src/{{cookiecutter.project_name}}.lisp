602af3137b3ad30ef14eb29f429c13d24acdfe28
vindarel <vindarel@mailz.org>
2021-06-22 20:03:42 +0200

    "parse" CLI args from a common function for Roswell and (main)

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_name}}.lisp
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_name}}.lisp
@@ -5,10 +5,24 @@
 (defun greet (&optional (name "{{ cookiecutter.author }}"))
   (format t "Hello ~a from ~a!~&" name "{{ cookiecutter.project_name }}"))
 
+(defun help ()
+  (format t "~&Usage:
+
+  {{ cookiecutter.project_name }} [name]~&"))
+
+(defun %main (argv)
+  "Parse CLI args."
+  (when (member "-h" argv :test #'equal)
+    ;; To properly parse command line arguments, use a third-party library such as
+    ;; unix-opts, defmain, adopt… when needed.
+    (help)
+    (uiop:quit))
+  (greet  (or (first argv)
+              "dear lisp user")))
+
 (defun main ()
   "Entry point for the executable.
-  Reads a command line argument."
+  Reads command line arguments."
   ;; uiop:command-line-arguments returns a list of arguments (sans the script name).
-  ;; Use a library for proper parsing (when needed).
-  (greet (or (first (uiop:command-line-arguments))
-             "{{ cookiecutter.author }}")))
+  ;; We defer the work of parsing to %main because we call it also from the Roswell script.
+  (%main (uiop:command-line-arguments)))
